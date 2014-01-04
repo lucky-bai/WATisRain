59f795e841bbeaa58ec6d0b75be8ae02675648c2
luckyt <bai.li.2005@gmail.com>
2014-01-03 19:41:31 -0700

    draws big map, cannot scroll or zoom

--- workspace/WATisRain/gen/com/example/watisrain/R.java
+++ workspace/WATisRain/gen/com/example/watisrain/R.java
@@ -22,9 +22,11 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int map=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int imageView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
