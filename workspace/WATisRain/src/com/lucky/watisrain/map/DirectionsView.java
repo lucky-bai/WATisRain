8824de42f850ff91ab8024a260134b72caaa4763
luckyt <bai.li.2005@gmail.com>
2014-07-01 17:30:40 -0400

    changing DirectionView to extend LinearLayout instead of TextView

--- workspace/WATisRain/src/com/lucky/watisrain/map/DirectionsView.java
+++ workspace/WATisRain/src/com/lucky/watisrain/map/DirectionsView.java
@@ -2,23 +2,25 @@ package com.lucky.watisrain.map;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /*
  * The widget that displays the directions in a list
  */
-public class DirectionsView extends TextView {
+public class DirectionsView extends LinearLayout {
 	
-	//TextView textview;
+	TextView textview;
 
 	public DirectionsView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		//textview = new TextView(context);
+		textview = new TextView(context);
+		textview.setText("Touch the map to select a destination");
+		addView(textview);
 	}
 	
-	/*
 	public void setText(CharSequence text){
 		textview.setText(text);
-	}*/
+	}
 	
 }
