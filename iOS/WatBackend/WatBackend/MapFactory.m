59d942865840a45b2b753604bbc44b7e48736f8a
Bai Li <luckytoilet@gmail.com>
2015-05-19 23:28:15 -0700

    ported some parsing code

--- iOS/WatBackend/WatBackend/MapFactory.m
+++ iOS/WatBackend/WatBackend/MapFactory.m
@@ -5,16 +5,59 @@
 
 @implementation MapFactory
 
-+ (Map*)readMapFromPath: (NSString*) path{
-    
+NSScanner *preprocess(NSString *str){
+    NSMutableString *poststr = [[NSMutableString alloc] init];
+    for(NSString *line in [str componentsSeparatedByString:@"\n"]){
+        NSString *line2 = [line stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+        if([line2 length] == 0) continue;
+        if([line2 characterAtIndex:0] == '#') continue;
+        [poststr appendString:line2];
+        [poststr appendString:@"\n"];
+    }
+    return [NSScanner scannerWithString:poststr];
+}
+
+void handleCommandLocation(Map *map, NSScanner *scanner){
+    NSLog(@"1\n");
+}
+
+void handleCommandPassiveLocation(Map *map, NSScanner *scanner){
+    NSLog(@"2\n");
+}
+
+void handleCommandPath(Map *map, NSScanner *scanner){
+    NSLog(@"3\n");
+}
+
+Map *readMapFromScanner(NSScanner *scanner){
     Map *map = [[Map alloc] init];
+    
+    while(!scanner.atEnd){
+        NSString *command;
+        if([scanner scanString:@"location" intoString:&command]){
+            handleCommandLocation(map, scanner);
+        }else if([scanner scanString:@"passive_location" intoString:&command]){
+            handleCommandPassiveLocation(map, scanner);
+        }else if([scanner scanString:@"path" intoString:&command]){
+            handleCommandPath(map, scanner);
+        }else{
+            scanner.scanLocation++;
+        }
+    }
+    
+    return map;
+}
+
++ (Map*)readMapFromPath: (NSString*) path{
     
     NSError *error;
     NSString *content = [[NSString alloc] initWithContentsOfFile:path encoding:NSUTF8StringEncoding error:&error];
     if(content == nil){
         NSLog(@"%@\n", error);
     }
-    NSLog(@"%@\n", content);
+    
+    NSScanner *stream = preprocess(content);
+    Map *map = readMapFromScanner(stream);
     
     return map;
 }
--- iOS/WatBackend/WatBackend/main.m
+++ iOS/WatBackend/WatBackend/main.m
@@ -13,16 +13,6 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         [MapFactory readMapFromPath:@"/Users/bai-personal/Documents/WATisRain/deprecated/locations.txt"];
-        
-        NSMutableArray *A = [[NSMutableArray alloc] init];
-        for(int i=1; i<10; i++){
-            [A addObject:[NSNumber numberWithInt:i]];
-            [A addObject:[NSNumber numberWithInt:-i]];
-        }
-        
-        A = (NSMutableArray*)[A sortedArrayUsingSelector:@selector(compare:)];
-        NSLog(@"%@", A);
-        
     }
     return 0;
 }
