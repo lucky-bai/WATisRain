4300d1069a09feeb855bd97b8387476a913bf5d3
Bai Li <luckytoilet@gmail.com>
2015-06-08 18:23:27 -0700

    all buildings drawn on map

--- iOS/WATisRain/WATisRain/MapView.h
+++ iOS/WATisRain/WATisRain/MapView.h
@@ -3,10 +3,21 @@
 #define WATisRain_MapView_h
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import "Map.h"
+
+static const float MAP_WIDTH = 2048.0f;
+static const float MAP_HEIGHT = 964.0f;
+static const float MAP_ADJUST_X = 611.0f;
+static const float MAP_ADJUST_Y = 773.0f;
+static const float MAP_ADJUST_SCALING = 0.8156f;
 
 @interface MapView : UIView
 
 @property UIImage *image;
+@property Map *map;
+
+@property UIImage *defaultLocationImage;
+@property UIImage *activeLocationImage;
 
 - (MapView*)initWithImage:(UIImage*)image;
 
--- iOS/WATisRain/WATisRain/MapView.m
+++ iOS/WATisRain/WATisRain/MapView.m
@@ -1,28 +1,39 @@
 
 #import <Foundation/Foundation.h>
 #import "MapView.h"
+#import "MapFactory.h"
+#import "Building.h"
 
 @implementation MapView
 
 - (MapView*)initWithImage:(UIImage*)image{
     _image = image;
     self = [super initWithFrame:CGRectMake(0, 0, image.size.width, image.size.height)];
+    
+    _defaultLocationImage = [UIImage imageNamed:@"default_location.png"];
+    _activeLocationImage = [UIImage imageNamed:@"active_location.png"];
+    
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"locations" ofType:@"txt" inDirectory:@""];
+    _map = [MapFactory readMapFromPath:path];
+    
     return self;
+}
+
+void drawImageOnMap(CGContextRef context, UIImage *img, float x_, float y_, float w_){
+    float x = (x_ - MAP_ADJUST_X) * MAP_ADJUST_SCALING;
+    float y = (y_ - MAP_ADJUST_Y) * MAP_ADJUST_SCALING;
+    float w = w_ * MAP_ADJUST_SCALING;
+    
+    [img drawInRect:CGRectMake(x-w/2.0, y-w/2.0, w, w)];
 }
 
 - (void)drawRect:(CGRect)rect{
     CGContextRef context = UIGraphicsGetCurrentContext();
     [self.image drawAtPoint:CGPointMake(0, 0)];
-    
-    // Can draw stuff!
-    CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
-    CGContextSetLineWidth(context, 3.0f);
-    CGContextMoveToPoint(context, 0, 0);
-    CGContextAddLineToPoint(context, self.image.size.width, self.image.size.height);
-    CGContextStrokePath(context);
-    CGContextMoveToPoint(context, self.image.size.width, 0);
-    CGContextAddLineToPoint(context, 0, self.image.size.height);
-    CGContextStrokePath(context);
+
+    for(Building *building in [_map buildings]){
+        drawImageOnMap(context, _defaultLocationImage, [[[building getMainFloor] position] x], [[[building getMainFloor] position] y], 120);
+    }
 }
 
 @end
